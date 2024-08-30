756fac95d08df0f1784163eb064039d6e394d7bc
miltonbhowmick <miltonbhowmick6@gmail.com>
2024-08-30 19:17:34 +0600

    feat: add order apis

---
+++ src/services/order.service.js
@@ -0,0 +1,40 @@
+import axios from "axios";
+import { PUBLIC_COMMON } from "@/utils/store/endpoints";
+import { buildParams } from "@/utils/utils";
+
+export const fetchOrders = async (params = {}, token) => {
+  return new Promise((resolve, reject) => {
+    axios({
+      method: "get",
+      headers: {
+        ...token,
+      },
+      url: `${PUBLIC_ORDER}/orders/${buildParams(params)}`,
+    })
+      .then((response) => {
+        resolve(response.data);
+      })
+      .catch((error) => {
+        reject(error.response?.data);
+      });
+  });
+};
+
+export const createOrder = async (payload = {}, token) => {
+  return new Promise((resolve, reject) => {
+    axios({
+      method: "post",
+      url: `${PUBLIC_ORDER}/orders/`,
+      headers: {
+        ...token,
+      },
+      data: payload,
+    })
+      .then((response) => {
+        resolve(response.data);
+      })
+      .catch((error) => {
+        reject(error.response?.data);
+      });
+  });
+};
--- src/utils/store/endpoints.js
+++ src/utils/store/endpoints.js
@@ -2,7 +2,6 @@ export const PUBLIC_API_ROOT = process.env.NEXT_PUBLIC_API_ROOT;
 
 export const PUBLIC_ACCOUNT = PUBLIC_API_ROOT + "/api/v1/account/public";
 export const USERS = PUBLIC_ACCOUNT + "/users";
-
 export const PUBLIC_COMMON = PUBLIC_API_ROOT + "/api/v1/common/public";
-
 export const PUBLIC_MUSIC = PUBLIC_API_ROOT + "/api/v1/music/public";
+export const PUBLIC_ORDER = PUBLIC_API_ROOT + "/api/v1/order/public";
