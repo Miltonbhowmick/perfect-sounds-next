34901e83924d24509b60bf7c2e7f2e594d7e4559
miltonbhowmick <miltonbhowmick6@gmail.com>
2024-09-07 01:24:26 +0600

    fix: bottom music designs issues

--- src/components/music/bottom-player.js
+++ src/components/music/bottom-player.js
@@ -1,5 +1,5 @@
 "use client";
-import { useEffect, useMemo, useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import {
   setCurrentVolume,
@@ -9,14 +9,12 @@ import {
 import ButtonMediaPlay from "@/components/button/mediaPlay";
 import WaveSurfer from "wavesurfer.js";
 import { useMediaElement } from "@/contexts/MediaElementContext";
-import { usePathname } from "next/navigation";
 
 import SkipPreviousIcon from "@mui/icons-material/SkipPrevious";
 import SkipNextIcon from "@mui/icons-material/SkipNext";
 import PlayArrowIcon from "@mui/icons-material/PlayArrow";
 
 const BottomPlayer = () => {
-  const path = usePathname();
   const dispatch = useDispatch();
   const currentMusic = useSelector((state) => {
     return state.music.currentMusic;
@@ -115,6 +113,13 @@ const BottomPlayer = () => {
     return `${minutes}:${secs < 10 ? "0" : ""}${secs}`;
   };
 
+  const getShortTitle = (title) => {
+    if (title.length > 30) {
+      return title.substring(0, 30) + "..";
+    }
+    return title;
+  };
+
   return (
     <div
       className="fixed z-[45] bottom-0 left-0 right-0 bg-[#000000] bg-opacity-70 overflow-hidden"
@@ -148,21 +153,8 @@ const BottomPlayer = () => {
             <div ref={bottomWaveformRef}></div>
           </div> */}
           <div className="w-full basis-auto lg:basis-[60%] shrink flex gap-2 md:gap-8 justify-start items-center">
-            <div className="flex gap-1 justify-between items-center">
-              <SkipPreviousIcon className="w-[18px] h-[18px] md:w-[60px] md:h-[60px] text-primaryText" />
-              <ButtonMediaPlay
-                className="w-[40px] h-[40px] md:w-[60px] md:h-[60px] rounded-full cursor-pointer"
-                iconColor="primaryText"
-                playing={playing}
-                gradient
-                onClick={handlePlayPause}
-              />
-              <SkipNextIcon className="w-[18px] h-[18px] md:w-[60px] md:h-[60px] text-primaryText" />
-              <button className="w-max px-1 md:px-4 h-[24px] md:h-[45px] flex gap-1 items-center rounded-full border border-primaryText">
-                <PlayArrowIcon className="text-primaryText text-small-xs md:text-paragraph-md" />
-                <p className="text-primaryText">Play All</p>
-              </button>
-              <div className="flex gap-2">
+            <div className="w-full flex gap-1 justify-between items-center">
+              <div className="flex basis-[40%] shrink lg:hidden gap-2">
                 <div>
                   {isMuted ? (
                     <svg
@@ -205,22 +197,99 @@ const BottomPlayer = () => {
                   min="0"
                   max="1"
                   value={currentVolume}
-                  step=".2"
+                  step=".005"
+                  className="w-full accent-gradientRight outline-none"
+                  onChange={(e) => handleCurrentVolume(e)}
+                />
+              </div>
+              <div className="flex basis-[30%] gap-1 justify-between items-center">
+                <SkipPreviousIcon className="w-[24px] h-[24px] md:w-[60px] md:h-[60px] text-primaryText" />
+                <ButtonMediaPlay
+                  className="w-[40px] h-[40px] md:w-[60px] md:h-[60px] rounded-full cursor-pointer"
+                  iconColor="primaryText"
+                  playing={playing}
+                  gradient
+                  onClick={handlePlayPause}
+                />
+                <SkipNextIcon className="w-[24px] h-[24px] md:w-[60px] md:h-[60px] text-primaryText" />
+              </div>
+              <button className="w-max px-2 md:px-4 h-[30px] md:h-[45px] flex gap-1 items-center rounded-full border border-primaryText">
+                <PlayArrowIcon className="text-primaryText text-small-xs md:text-paragraph-lg" />
+                <p className="text-primaryText">Play All</p>
+              </button>
+              <div className="hidden lg:flex gap-2">
+                <div>
+                  {isMuted ? (
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 28 24"
+                      fill="none"
+                      className="w-[24px] h-[24px] md:w-[30px] md:h-[30px]"
+                      onClick={toggleMute}
+                    >
+                      <path
+                        d="M14.4102 0.890625C14.2812 0.960938 12.5586 2.22656 10.5898 3.69141L7.00391 6.375H4.96484C2.67969 6.375 2.31641 6.46875 1.61328 7.20703C0.898438 7.96875 0.875 8.10938 0.875 12.0234C0.875 15.9844 0.898438 16.125 1.70703 16.8867C2.41016 17.543 2.75 17.625 4.98828 17.625H7.00391L10.5898 20.3086C12.5586 21.7852 14.293 23.0508 14.4336 23.1211C14.5742 23.1914 15.0195 23.25 15.418 23.25C16.3438 23.25 17.0234 22.8867 17.457 22.1484L17.75 21.6445V12V2.35547L17.457 1.85156C17.0234 1.11328 16.3438 0.75 15.3945 0.75C14.9844 0.75 14.5391 0.820312 14.4102 0.890625Z"
+                        fill="white"
+                      />
+                      <path
+                        d="M19.9414 8.39056C19.6367 8.62494 19.4961 9.14056 19.6367 9.50384C19.6953 9.69134 20.2344 10.3242 20.832 10.9101L21.9101 11.9999L20.832 13.0781C20.2344 13.6757 19.6953 14.3085 19.6367 14.4843C19.3555 15.246 20.1172 16.0312 20.8789 15.7382C21.0547 15.6796 21.6992 15.1406 22.2969 14.5429L23.375 13.4648L24.4648 14.5429C25.0508 15.1406 25.6953 15.6796 25.8711 15.7382C26.6328 16.0312 27.3945 15.246 27.1133 14.4843C27.0547 14.3085 26.5156 13.6757 25.918 13.0781L24.8398 11.9999L25.918 10.9101C27.1133 9.71478 27.3008 9.38666 27.1133 8.83587C26.9609 8.40228 26.6914 8.24994 26.1172 8.24994C25.7305 8.24994 25.5664 8.36712 24.5351 9.38666L23.375 10.5234L22.2148 9.38666C21.1484 8.33197 21.0312 8.24994 20.5976 8.24994C20.3515 8.24994 20.0469 8.32025 19.9414 8.39056Z"
+                        fill="white"
+                      />
+                    </svg>
+                  ) : (
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 22 24"
+                      fill="none"
+                      className="w-[24px] h-[24px] md:w-[30px] md:h-[30px]"
+                      onClick={toggleMute}
+                    >
+                      <path
+                        d="M11.7032 0.937378C11.4688 1.04285 10.2383 2.15613 8.77349 3.62097C5.87895 6.50378 5.60942 6.67957 3.87505 6.84363C2.36333 6.98425 1.70708 7.33582 1.19145 8.2616C1.00395 8.61316 0.980517 9.0116 0.94536 11.7303C0.921923 13.7343 0.957079 14.9647 1.03911 15.2811C1.20317 15.8788 1.97661 16.746 2.48052 16.91C2.69145 16.9686 3.28911 17.0741 3.82817 17.1444C5.53911 17.3671 5.71489 17.4843 8.5977 20.3319C10.0157 21.7264 11.3399 22.9569 11.5508 23.0624C12.0665 23.3202 13.0157 23.2968 13.5547 23.0272C14.4571 22.5702 14.6797 22.0897 14.9727 19.9569C15.7813 14.1796 15.8633 9.78504 15.2657 4.91003C14.9141 2.06238 14.6094 1.27722 13.7422 0.91394C13.2383 0.703003 12.2188 0.714722 11.7032 0.937378Z"
+                        fill="white"
+                      />
+                      <path
+                        d="M18.0664 4.67557C17.6446 4.96854 17.6094 5.57792 17.9492 6.46854C18.6758 8.3201 18.9102 10.0779 18.8164 12.7498C18.7344 14.742 18.5821 15.6912 18.0781 17.074C17.9141 17.5428 17.75 18.1404 17.7149 18.4099C17.668 18.8435 17.7031 18.949 17.9727 19.2068C18.4414 19.6404 19.086 19.617 19.4727 19.1482C20.4336 18.0115 21.2071 14.0154 21.0313 11.0974C20.8672 8.35526 20.0586 5.28495 19.3438 4.72245C18.9922 4.45292 18.418 4.42948 18.0664 4.67557Z"
+                        fill="white"
+                      />
+                    </svg>
+                  )}
+                </div>
+                <input
+                  type="range"
+                  min="0"
+                  max="1"
+                  value={currentVolume}
+                  step=".005"
                   className="accent-gradientRight outline-none"
                   onChange={(e) => handleCurrentVolume(e)}
                 />
               </div>
             </div>
-            <div className="hidden lg:block w-full">
+            <div className="hidden">
               <div ref={bottomWaveformRef}></div>
             </div>
+            <div className="hidden lg:flex w-full gap-5">
+              <input
+                type="range"
+                min="0"
+                max={duration}
+                step="1"
+                onChange={(e) => {
+                  handleCurrentPlayingTime(e);
+                }}
+                className="accent-gradientRight w-full bg-[#F47B23]"
+              />
+            </div>
           </div>
-          <div className="border-t border-[#4F4F4F] w-full basis-auto lg:basis-[35%] pt-2 lg:py-auto flex gap-3 justify-between items-center">
+          <div className="border-t border-[#4F4F4F] lg:border-none w-full basis-auto lg:basis-[35%] pt-2 lg:py-auto flex gap-3 justify-between items-center">
             <div className="flex flex-col ga-1">
               <p className="text-[10px] sm:text-[16px] uppercase text-primaryText">
                 Now playing:
               </p>
-              <p className="text-primaryText">{currentMusic?.name}</p>
+              <p className="text-primaryText">
+                {getShortTitle(currentMusic?.title)}
+              </p>
             </div>
             <div className="flex gap-3 justify-between items-center">
               <div className="hidden lg:block text-primaryText flex gap-2 items-center">
